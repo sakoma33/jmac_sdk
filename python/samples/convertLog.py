e7ff9017d21d0166d6ced1029b573c8f9e4d8fd6
mono-1729 <kawai.172957@gmail.com>
2023-03-01 13:56:17 +0900

    役名の表示

--- python/samples/convertLog.py
+++ python/samples/convertLog.py
@@ -1,10 +1,15 @@
 """
 Todo:
-  - 包（パオ）などの特殊状況の処理
-  - 点数と役名の表示の追加
+  - 包（パオ）、などの特殊状況の処理
   - （暗槓時のaの位置と赤の処理について調べる）
 """
 import json
+
+BASE_URL ="https://tenhou.net/5/#json="
+convert_id = lambda id: 50+id//36+1 if id in [16, 52, 88] else (id//36+1)*10+(id%36)//4+1
+ten_name = {8000:"満貫",12000:"跳満",16000:"倍満",24000:"三倍満"}
+yaku_dict = {0:"門前清自摸和",1:"立直",2:"一発",3:"槍槓",4:"嶺上開花",5:"海底摸月",6:"河底撈魚",7:"平和",8:"断幺九",9:"一盃口",10:"自風 東",11:"自風 南",12:"自風 西",13:"自風 北",14:"場風 東",15:"場風 南",16:"場風 西",17:"場風 北",18:"役牌 白",19:"役牌 發",20:"役牌 中",21:"両立直",22:"七対子",23:"混全帯幺九",24:"一気通貫",25:"三色同順",26:"三色同刻",27:"三槓子",28:"対々和",29:"三暗刻",30:"小三元",31:"混老頭",32:"二盃口",33:"純全帯幺九",34:"混一色",35:"清一色",36:"人和",37:"天和",38:"地和",39:"大三元",40:"四暗刻",41:"四暗刻単騎",42:"字一色",43:"緑一色",44:"清老頭",45:"九蓮宝燈",46:"純正九蓮宝燈",47:"国士無双",48:"国士無双１３面",49:"大四喜",50:"小四喜",51:"四槓子",52:"ドラ",53:"裏ドラ",54:"赤ドラ"}
+
 class ConvertLog:
 	"""
 	半荘分のログを天鳳の形式に変換し、URLを生成する。
@@ -14,20 +19,18 @@ class ConvertLog:
 	get_url(self)
 		天鳳の牌譜URLを返す(str型)
 	"""
-	BASE_URL = "https://tenhou.net/5/#json="
 	def __init__(self):
 		self.logs = { "title":["",""], "name":["","","",""], "rule":{"disp":"","aka":1}, "log":[]}
 
 	def add_log(self,obs_dict):
-		convert_id = lambda id: 50+id//36+1 if id in [16, 52, 88] else (id//36+1)*10+(id%36)//4+1
 		log = [[0,0,0],[0,0,0,0],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
 		for i,obs in enumerate(obs_dict.values()):
 			obs=json.loads(obs.to_json())
 			if i == 0:
 				self.logs["name"] = obs["publicObservation"]["playerIds"]
 				
-				if "round" in obs["publicObservation"]["initScore"].keys():
-					log[0][0] = obs["publicObservation"]["initScore"]["round"]
+				round = obs["publicObservation"]["initScore"]["round"] if "round" in obs["publicObservation"]["initScore"].keys() else 0
+				log[0][0] = round
 
 				if "honba" in obs["publicObservation"]["initScore"].keys():
 					log[0][1] = obs["publicObservation"]["initScore"]["honba"]
@@ -48,7 +51,29 @@ class ConvertLog:
 							log[3] = win["uraDoraIndicators"]
 						who_win = win["who"] if "who" in win.keys() else 0
 						from_who_win = win["fromWho"] if "fromWho" in win.keys() else 0
-						log.append(["和了",win["tenChanges"],[who_win,from_who_win,who_win,str(win["ten"])+"点"]])
+						agari_info=[who_win,from_who_win,who_win]
+						ten = win["ten"]
+						magnification = 1
+						if round%4 == who_win:
+							magnification = 1.5
+						adjusted_ten = int(ten//magnification)
+						if adjusted_ten < 8000:
+							fan = sum(win["fans"])
+							fu = win["fu"]
+							agari_info.append(f"{fan}飜{fu}符{ten}点")
+						elif adjusted_ten < 32000:
+							agari_info.append(f"{ten_name[int(adjusted_ten)]}{ten}点")
+						else:
+							agari_info.append(f"役満{ten}点")
+						if "yakus" in win.keys():
+							for i,yaku in enumerate(win["yakus"]):
+								fan = win["fans"][i]
+								if fan>0:
+									agari_info.append(f"{yaku_dict[yaku]}({fan}飜)")
+						else:
+							for yaku in win["yakumans"]:
+								agari_info.append(f"{yaku_dict[yaku]}(役満)")
+						log.append(["和了",win["tenChanges"],agari_info])
 			who=obs["who"] if "who" in obs.keys() else 0
 			log[4+who*3] = [convert_id(id) for id in obs["privateObservation"]["initHand"]["closedTiles"]]
 			tumo_count=0
@@ -93,7 +118,7 @@ class ConvertLog:
 					elif event["type"] == "EVENT_TYPE_PON":
 						open = event["open"]
 						mask_from = open%4
-						pon_unused_offset = ((0b0000000001100000 & open)>>5)%3
+						pon_unused_offset = ((0b0000000001100000 & open)>>5)%4
 						pon_base_and_stolen = (0b1111111000000000 & open)>>9
 						pon_base = (pon_base_and_stolen//3)*4
 						stolen = pon_base_and_stolen%3
@@ -118,7 +143,7 @@ class ConvertLog:
 					elif event["type"]=="EVENT_TYPE_ADDED_KAN":
 						open = event["open"]
 						mask_from = open%4
-						pon_unused_offset = ((0b0000000001100000 & open)>>5)%3
+						pon_unused_offset = ((0b0000000001100000 & open)>>5)%4
 						pon_base_and_stolen = (0b1111111000000000 & open)>>9
 						pon_base = (pon_base_and_stolen//3)*4
 						stolen = pon_base_and_stolen%3
@@ -147,4 +172,4 @@ class ConvertLog:
 		self.logs["log"].append(log)
 	
 	def get_url(self):
-		return self.BASE_URL+json.dumps(self.logs, separators=(',', ':'),ensure_ascii=False)
+		return BASE_URL+json.dumps(self.logs, separators=(',', ':'),ensure_ascii=False)
--- python/samples/test.py
+++ python/samples/test.py
@@ -12,64 +12,8 @@ class RandomAgent(mjx.Agent):
         legal_actions: List[mjx.Action] = observation.legal_actions()
         return random.choice(legal_actions)
 
-class ShantenAgent(mjx.Agent):
-    """A rule-based agent, which plays just to reduce the shanten-number.
-    The logic is basically intended to reproduce Mjai's ShantenPlayer.
-    - Mjai https://github.com/gimite/mjai
-    """
 
-    def __init__(self) -> None:
-        super().__init__()
-
-    def act(self, observation: mjx.Observation) -> mjx.Action:
-        legal_actions = observation.legal_actions()
-        if len(legal_actions) == 1:
-            return legal_actions[0]
-
-        # if it can win, just win
-        win_actions = [a for a in legal_actions if a.type() in [mjx.ActionType.TSUMO, mjx.ActionType.RON]]
-        if len(win_actions) >= 1:
-            assert len(win_actions) == 1
-            return win_actions[0]
-
-        # if it can declare riichi, just declar riichi
-        riichi_actions = [a for a in legal_actions if a.type() == mjx.ActionType.RIICHI]
-        if len(riichi_actions) >= 1:
-            assert len(riichi_actions) == 1
-            return riichi_actions[0]
-
-        # if it can apply chi/pon/open-kan, choose randomly
-        steal_actions = [
-            a
-            for a in legal_actions
-            if a.type() in [mjx.ActionType.CHI, mjx.ActionType.PON, mjx.ActionType, mjx.ActionType.OPEN_KAN]
-        ]
-        if len(steal_actions) >= 1:
-            return random.choice(steal_actions)
-
-        # if it can apply closed-kan/added-kan, choose randomly
-        kan_actions = [
-            a for a in legal_actions if a.type() in [mjx.ActionType.CLOSED_KAN, mjx.ActionType.ADDED_KAN]
-        ]
-        if len(kan_actions) >= 1:
-            return random.choice(kan_actions)
-
-        # discard an effective tile randomly
-        legal_discards = [
-            a for a in legal_actions if a.type() in [mjx.ActionType.DISCARD, mjx.ActionType.TSUMOGIRI]
-        ]
-        effective_discard_types = observation.curr_hand().effective_discard_types()
-        effective_discards = [
-            a for a in legal_discards if a.tile().type() in effective_discard_types
-        ]
-        if len(effective_discards) > 0:
-            return random.choice(effective_discards)
-
-        # if no effective tile exists, discard randomly
-        return random.choice(legal_discards)
-
-
-agent = ShantenAgent()
+agent = RandomAgent()
 env = mjx.MjxEnv()
 N = 100
 urls=[]
