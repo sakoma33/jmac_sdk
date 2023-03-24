63caf28aaabe23f85b16c31f33d4490282b24700
sakoma33 <dev7618jifekd@gmail.com>
2023-03-24 22:57:30 +0900

    プルリクを出せるように整理

--- python/custom_ai.py
+++ python/custom_ai.py
@@ -1,6 +1,3 @@
-# import matplotlib.pyplot as plt
-# import seaborn as sns
-# import tensorflow as tf
 import glob
 import os
 
@@ -15,136 +12,6 @@ from pytorch_lightning import Trainer
 from torch import Tensor, nn, optim, utils
 from torch.utils.data import DataLoader, TensorDataset
 
-# from tensorflow import keras
-# from tensorflow.keras import layers
-
-
-class MLP(pl.LightningModule):
-    def __init__(self, obs_size=544, n_actions=181, hidden_size=128):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(obs_size, hidden_size),
-            nn.ReLU(),
-            nn.Linear(hidden_size, hidden_size),
-            nn.ReLU(),
-            nn.Linear(hidden_size, n_actions),
-        )
-        self.loss_module = nn.CrossEntropyLoss()
-
-    def training_step(self, batch, batch_idx):
-        x, y = batch
-        preds = self.forward(x)
-        loss = self.loss_module(preds, y)
-        self.log("train_loss", loss)
-        return loss
-
-    def configure_optimizers(self):
-        optimizer = optim.Adam(self.parameters(), lr=1e-3)
-        return optimizer
-
-    def forward(self, x):
-        return self.net(x.float())
-
-
-class MLP_large(pl.LightningModule):
-    def __init__(self, obs_size=4488, n_actions=181, hidden_size=544):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(obs_size, hidden_size),
-            nn.ReLU(),
-            nn.Linear(hidden_size, n_actions),
-        )
-        self.loss_module = nn.CrossEntropyLoss()
-
-    def training_step(self, batch, batch_idx):
-        x, y = batch
-        preds = self.forward(x)
-        loss = self.loss_module(preds, y)
-        self.log("train_loss", loss)
-        return loss
-
-    def forward(self, x):
-        return self.net(x.float())
-
-
-class MLP_small(pl.LightningModule):
-    def __init__(self, obs_size=544, n_actions=181, hidden_size=544):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(obs_size, hidden_size),
-            nn.ReLU(),
-            nn.Linear(hidden_size, n_actions),
-        )
-        self.loss_module = nn.CrossEntropyLoss()
-
-    def training_step(self, batch, batch_idx):
-        x, y = batch
-        preds = self.forward(x)
-        loss = self.loss_module(preds, y)
-        self.log("train_loss", loss)
-        return loss
-
-    def forward(self, x):
-        return self.net(x.float())
-
-
-# dataset = np.load("dataset/large-100.npy")
-# print(dataset.shape)
-# x = dataset[:, :-1]
-# y = dataset[:, -1]
-
-# dataset = TensorDataset(torch.Tensor(x), torch.LongTensor(y))
-# loader = DataLoader(dataset, batch_size=2)
-
-# model = MLP()
-# trainer = pl.Trainer(max_epochs=1)
-# trainer.fit(model=model, train_dataloaders=train_loader)
-# torch.save(model.state_dict(), './model_shanten_100.pth')
-
-
-# # 学習データに対する処理
-# class TrainNet(pl.LightningModule):
-
-#     @pl.data_loader
-#     def train_dataloader(self):
-#         return torch.utils.data.DataLoader(train, self.batch_size, shuffle=True)
-
-#     def training_step(self, batch, batch_nb):
-#         x, t = batch
-#         y = self.forward(x)
-#         loss = self.lossfun(y, t)
-#         results = {'loss': loss}
-#         return results
-
-# # ディレクトリのパスを指定
-# dir_path = "json"
-# data_set = []
-# # ディレクトリ直下のファイルを取得
-# files = glob.glob("./json/*")
-# # print(len(files))
-# for i, file in enumerate(files[:1000]):
-#     with open(file) as f:
-#         lines = f.readlines()
-#         for line in lines:
-#             state = State(line)
-#             for cpp_obs, cpp_act in state._cpp_obj.past_decisions():
-#                 obs = Observation._from_cpp_obj(cpp_obs)
-#                 # feature = obs.to_features(feature_name="mjx-small-v0")
-#                 feature = obs.to_features(feature_name="mjx-large-v0").ravel()
-
-#                 action = Action._from_cpp_obj(cpp_act)
-#                 action_idx = action.to_idx()
-#                 data = np.append(feature, action_idx)
-#                 data_set.append(data)
-
-# print(len(data_set))
-
-# data_set = np.array(data_set)
-# np.save("dataset/large-1000", data_set)
-# # data_set = np.load("dataset/large.npy")
-# print(str(data_set.shape))
-# # データをCSVファイルに保存する
-# # np.savetxt('dataset/large.csv', data_set, delimiter=',')
 
 def make_dataset():
     dir_path = "json"
@@ -273,25 +140,19 @@ class Net(TrainNet, ValidationNet, TestNet):
 if __name__ == "__main__":
     # make_dataset()
     dataset = np.load("dataset/small-1300.npy")
-    # dataset2 = np.load("dataset/large-100-200.npy")
-    # dataset3 = np.load("dataset/large-200-300.npy")
-    # dataset = np.concatenate([dataset1, dataset2])
-    print(dataset.shape)
     x = dataset[:, :-1]
     y = dataset[:, -1]
-    # y = np.array([int_to_binary_vector(element) for element in y])
-    y = np.eye(181)[y]           # one hot表現に変換
 
-    # print(x.shape)
-    # print(y.shape)
+    y = np.eye(181)[y]   # one hot表現に変換
+
     x = torch.tensor(x, dtype=torch.float32)
     y = torch.tensor(y, dtype=torch.float32)
     dataset = torch.utils.data.TensorDataset(x, y)
-    # print(dataset[0])
+
     n_train = int(len(dataset) * 0.7)
     n_val = int(len(dataset) * 0.2)
     n_test = len(dataset) - n_train - n_val
-    # print(n_train, n_val, n_test)
+
     # ランダムに分割を行うため、シードを固定して再現性を確保
     torch.manual_seed(0)
 
@@ -299,7 +160,6 @@ if __name__ == "__main__":
     train, val, test = torch.utils.data.random_split(
         dataset, [n_train, n_val, n_test])
 
-    # 再現性の確保
     torch.manual_seed(0)
 
     # インスタンス化
@@ -310,54 +170,7 @@ if __name__ == "__main__":
     trainer.fit(net)
 
     trainer.test()
-    # print(trainer.callback_metrics)
+
     # torch.save(net, 'models/second_model.pth')
     # model = torch.load("models/second_model.pth")
     torch.save(net.state_dict(), 'models/fifth_model.pth')
-
-    # def ai():
-
-    #     X_nn_train = X[:70]
-    #     y_nn_train = y[:70]
-    #     X_nn_valid = X[70:85]
-    #     y_nn_valid = y[70:85]
-    #     X_nn_test = X[85:100]
-    #     y_nn_test = y[85:100]
-    #     normalizer = tf.keras.layers.Normalization(axis=-1)
-    #     normalizer.adapt(np.array(X[:85]))
-
-    #     def build_and_compile_model(norm):
-    #         model = keras.Sequential([
-    #             norm,
-    #             layers.Dense(64, activation='relu'),
-    #             layers.Dense(64, activation='relu'),
-    #             layers.Dense(1)
-    #         ])
-
-    #         model.compile(loss='mean_squared_error',
-    #                       optimizer=tf.keras.optimizers.Adam(0.001),
-    #                       # metrics=['mean_squared_error']
-    #                       )
-    #         return model
-
-    #     nn_model = build_and_compile_model(normalizer)
-    #     nn_model.summary()
-
-    #     history = nn_model.fit(
-    #         X_nn_train,
-    #         y_nn_train,
-    #         validation_data=(X_nn_valid, y_nn_valid),
-    #         verbose=1, epochs=1000)
-
-    #     def plot_loss(history):
-    #         plt.plot(history.history['loss'], label='loss')
-    #         plt.plot(history.history['val_loss'], label='val_loss')
-    #         # plt.plot(np.log(np.array(history.history['loss'])), label='loss')
-    #         # plt.plot(np.log(np.array(history.history['val_loss'])), label='val_loss')
-    #         # plt.ylim([-0.1, 0.25])
-    #         plt.xlabel('Epoch')
-    #         plt.ylabel('')
-    #         plt.legend()
-    #         plt.grid(True)
-
-    #     plot_loss(history)
--- python/custom_client_sakoma33.py
+++ python/custom_client_sakoma33.py
@@ -1,8 +1,11 @@
 import random
 
 import mjx
+import torch
 from client.agent import CustomAgentBase
 from client.client import SocketIOClient
+from custom_ai import Net
+from torch import Tensor, nn, optim, utils
 
 
 # CustomAgentBase を継承して，
@@ -10,6 +13,9 @@ from client.client import SocketIOClient
 class MyAgent(CustomAgentBase):
     def __init__(self):
         super().__init__()
+        # self.model = torch.load('models/second_model.pth')
+        self.model = Net()
+        self.model.load_state_dict(torch.load("models/fourth_model.pth"))
 
     def custom_act(self, obs: mjx.Observation) -> mjx.Action:
         """盤面情報と取れる行動を受け取って，行動を決定して返す関数．参加者が各自で実装．
@@ -20,9 +26,40 @@ class MyAgent(CustomAgentBase):
         Returns:
             mjx.Action: 実際に取る行動
         """
-        print(random.choice(obs.legal_actions()))
-        # ランダムに取れる行動をする
-        return random.choice(obs.legal_actions())
+        # // 0~33: Discard m1~rd
+        # // 34,35,36: Discard m5(red), p5(red), s5(red)
+        # // 37~70: Tsumogiri m1~rd
+        # // 71,72,73: Tsumogiri m5(red), p5(red), s5(red)
+        # // 74~94: Chi m1m2m3 ~ s7s8s9
+        # // 95,96,97: Chi m3m4m5(red), m4m5(red)m6, m5(red)m6m7
+        # // 98,99,100: Chi p3p4p5(red), p4p5(red)p6, p5(red)p6p7
+        # // 101,102,103: Chi s3s4s5(red), s4s5(red)s6, s5(red)s6s7
+        # // 104~137: Pon m1~rd
+        # // 138,139,140: Pon m5(w/ red), s5(w/ red), p5(w/ red)
+        # // 141~174: Kan m1~rd
+        # // 175: Tsumo
+        # // 176: Ron
+        # // 177: Riichi
+        # // 178: Kyuushu
+        # // 179: No
+        # // 180: Dummy
+
+        legal_actions = obs.legal_actions()
+        if len(legal_actions) == 1:
+            return legal_actions[0]
+
+        # 予測
+        feature = obs.to_features(feature_name="mjx-small-v0")
+        # feature = obs.to_features(feature_name="mjx-large-v0")
+        self.model.eval()
+        with torch.no_grad():
+            action_logit = self.model(Tensor(feature.ravel()))
+        action_proba = torch.sigmoid(action_logit).numpy()
+
+        # アクション決定
+        mask = obs.action_mask()
+        action_idx = (mask * action_proba).argmax()
+        return mjx.Action.select_from(action_idx, legal_actions)
 
 
 if __name__ == "__main__":
